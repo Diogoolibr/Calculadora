0b5af78d4ec41d01b8467dbb3f391e2d6575f5f1
diogodossolivei <33028886-diogodossolivei@users.noreply.replit.com>
2024-03-24 18:36:18 +0000

    Initial commit

---
+++ Calculadora.js
@@ -0,0 +1,15 @@
+ function calcularRaizes(){
+  var a = document.getElementById("ValorA").value;
+  var b = document.getElementById("ValorB").value;
+  var c = document.getElementById("ValorC").value;
+  var delta = (b*b) - (4*a*c);
+
+  if (delta >= 0){
+    var x1 = (-b + Math.sqrt(delta))/(2*a);
+    var x2 = (-b - Math.sqrt(delta))/(2*a);
+    document.getElementById("resultado").innerHTML = "Resultado: Raizes x1 = " + x1 + " e x2 = " + x2;
+  } else {
+    document.getElementById("resultado").innerHTML = "Resultado: A equação não possui raizes reais";
+  }
+}
+     
