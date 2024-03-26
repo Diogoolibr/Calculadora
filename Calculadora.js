06e87ddde3f14f9102579958a9eb9c43574a275a
Diogoolibr <156860214+Diogoolibr@users.noreply.github.com>
2024-03-26 22:27:06 +0000

    Atualização v1.1

--- Calculadora.js
+++ Calculadora.js
@@ -1,4 +1,4 @@
- function calcularRaizes(){
+function calcularRaizes(){
   var a = document.getElementById("ValorA").value;
   var b = document.getElementById("ValorB").value;
   var c = document.getElementById("ValorC").value;
@@ -12,4 +12,59 @@
     document.getElementById("resultado").innerHTML = "Resultado: A equação não possui raizes reais";
   }
 }
+
+function exibirParabola() {
+  var canvas = document.getElementById("parabolacanvas");
+  var context = canvas.getContext("2d");
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  var a = parseFloat(document.getElementById("ValorA").value);
+  var b = parseFloat(document.getElementById("ValorB").value);
+  var c = parseFloat(document.getElementById("ValorC").value);
+  context.strokeStyle = "blue";
+  context.lineWidth = 2;
+      
+  context.beginPath();
+      
+  for (var x = -10; x <= 10; x += 0.1){
+    var y = a*x*x + b*x + c;
+    var pixelX = (x + 10) * (canvas.width / 20);
+    var pixelY = canvas.height - (y + 10) * (canvas.height / 20);
+
+    if (x == -10) {
+      context.moveTo(pixelX, pixelY);
+    } else {
+      context.lineTo(pixelX, pixelY);
+    }
+  }
+
+  context.stroke();
+  context.closePath();
+  desenharEixos(canvas);
+}
+
+function desenharEixos(canvas) {
+  var context = canvas.getContext("2d");
+
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.beginPath();
+  
+  context.moveTo(0, canvas.height / 2);
+  context.lineTo(canvas.width, canvas.height / 2);
+  context.stroke();
+  context.closePath();
+
+  context.beginPath();
+  context.moveTo(canvas.width / 2, 0);
+  context.lineTo(canvas.width / 2, canvas.height);
+  
+  context.stroke();
+  context.closePath();
+
+  }
+
+  exibirParabola();
      
+
