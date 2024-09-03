b68916e35cc0b1b5463bcd69ecaf298680ded57f
Diogoolibr <diogodoss.oliveira@gmail.com>
2024-09-03 17:39:04 -0400

    formatação

--- Calculadora.js
+++ Calculadora.js
@@ -1,15 +1,17 @@
-function calcularRaizes(){
+function calcularRaizes() {
   var a = document.getElementById("ValorA").value;
   var b = document.getElementById("ValorB").value;
   var c = document.getElementById("ValorC").value;
-  var delta = (b*b) - (4*a*c);
+  var delta = b * b - 4 * a * c;
 
-  if (delta >= 0){
-    var x1 = (-b + Math.sqrt(delta))/(2*a);
-    var x2 = (-b - Math.sqrt(delta))/(2*a);
-    document.getElementById("resultado").innerHTML = "Resultado: Raizes x1 = " + x1 + " e x2 = " + x2;
+  if (delta >= 0) {
+    var x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    var x2 = (-b - Math.sqrt(delta)) / (2 * a);
+    document.getElementById("resultado").innerHTML =
+      "Resultado: Raizes x1 = " + x1 + " e x2 = " + x2;
   } else {
-    document.getElementById("resultado").innerHTML = "Resultado: A equação não possui raizes reais";
+    document.getElementById("resultado").innerHTML =
+      "Resultado: A equação não possui raizes reais";
   }
 }
 
@@ -24,11 +26,11 @@ function exibirParabola() {
   var c = parseFloat(document.getElementById("ValorC").value);
   context.strokeStyle = "blue";
   context.lineWidth = 2;
-      
+
   context.beginPath();
-      
-  for (var x = -10; x <= 10; x += 0.1){
-    var y = a*x*x + b*x + c;
+
+  for (var x = -10; x <= 10; x += 0.1) {
+    var y = a * x * x + b * x + c;
     var pixelX = (x + 10) * (canvas.width / 20);
     var pixelY = canvas.height - (y + 10) * (canvas.height / 20);
 
@@ -50,7 +52,7 @@ function desenharEixos(canvas) {
   context.strokeStyle = "black";
   context.lineWidth = 1;
   context.beginPath();
-  
+
   context.moveTo(0, canvas.height / 2);
   context.lineTo(canvas.width, canvas.height / 2);
   context.stroke();
@@ -59,12 +61,9 @@ function desenharEixos(canvas) {
   context.beginPath();
   context.moveTo(canvas.width / 2, 0);
   context.lineTo(canvas.width / 2, canvas.height);
-  
+
   context.stroke();
   context.closePath();
+}
 
-  }
-
-  exibirParabola();
-     
-
+exibirParabola();
